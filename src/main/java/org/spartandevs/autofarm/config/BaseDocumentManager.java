267ec9cc8f707df1ba46a6c8b8aebae04a6b401d
sb2bg <sullivanbognar@gmail.com>
2023-06-17 18:18:51 -0500

    Create BaseDocumentManager to abstract yaml files

---
+++ src/main/java/org/spartandevs/autofarm/config/BaseDocumentManager.java
@@ -0,0 +1,79 @@
+package org.spartandevs.autofarm.config;
+
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
+import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
+import org.spartandevs.autofarm.AutoFarm;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public abstract class BaseDocumentManager {
+    protected final AutoFarm plugin;
+    protected YamlDocument document;
+    private final String resource;
+
+    public BaseDocumentManager(AutoFarm plugin, String resource) {
+        this.plugin = plugin;
+        this.resource = resource;
+        loadDocument();
+    }
+
+    protected void loadDocument() {
+        InputStream resourceStream = plugin.getResource(resource);
+
+        if (resourceStream == null) {
+            errorAndDisable("Could not find resource " + resource);
+            return;
+        }
+
+        if (!sneaky(() -> document = YamlDocument.create(
+                new File(plugin.getDataFolder(), resource),
+                resourceStream,
+                GeneralSettings.DEFAULT,
+                LoaderSettings.builder().setAutoUpdate(true).build(),
+                DumperSettings.DEFAULT,
+                updater()))) {
+            errorAndDisable("Could not load document " + resource);
+        }
+    }
+
+    protected abstract UpdaterSettings updater();
+
+    interface ConfigAction {
+        void run() throws IOException;
+    }
+
+    protected boolean sneaky(ConfigAction action) {
+        try {
+            action.run();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    protected void save() {
+        if (sneaky(document::save)) {
+            return;
+        }
+
+        plugin.getLogger().severe("Could not save " + resource);
+    }
+
+    public void reload() {
+        if (sneaky(document::reload)) {
+            return;
+        }
+
+        plugin.getLogger().severe("Could not reload " + resource);
+    }
+
+    protected void errorAndDisable(String message) {
+        plugin.getLogger().severe(message);
+        plugin.getServer().getPluginManager().disablePlugin(plugin);
+    }
+}
